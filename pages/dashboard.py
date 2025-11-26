6d93726b79cfbd7f05bbe25bb29a11b29cae4b26
vivek giri <130244229+vivek7557@users.noreply.github.com>
2025-11-26 23:33:41 +0530

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,6 +2,9 @@ import streamlit as st
 from pathlib import Path
 import json
 import os
+from ui.theme import load_theme
+load_theme()
+
 
 def dashboard_page():
     st.markdown("<h1 class='page-title'>Dashboard</h1>", unsafe_allow_html=True)
